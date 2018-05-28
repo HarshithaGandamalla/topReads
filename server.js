955396e654f230ef9fae4b2a753ebb4f1fdb1666
Harshitha <harshithag15@gmail.com>
2018-05-27 22:01:37 -0400

    Changed 15 to 12

--- server.js
+++ server.js
@@ -10,21 +10,10 @@ const api = require('./server/routes/api');
 
 const app = express();
 
-//var books={};
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, '/dist')));
-
-app.use('/api', api);
-
-//    
-//fetch('https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key=7f0f22c09d544373bfccc451fa6f76bb', {
-//    method: 'get',
-//  })
-//  .then(response => { return response.json()});
-
-    
 
  
 // Catch all other routes and return the index file
--- src/app/model.ts
+++ src/app/model.ts
@@ -1,14 +1,14 @@
-export class Book {
-  name: string;
-  status: string;
-  about: string;
-  messages: Message[];
-
-  rows: number;
-  cols: number;
-}
-
-export class Message {
-  who: string;
-  content: string;
-}
+//export class Book {
+//  name: string;
+//  status: string;
+//  about: string;
+//  messages: Message[];
+//
+//  rows: number;
+//  cols: number;
+//}
+//
+//export class Message {
+//  who: string;
+//  content: string;
+//}
